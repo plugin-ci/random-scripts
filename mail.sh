8a762c98b7b53b8b978346db3a87ce4f14485b17
Goblom <goblom@me.com>
2013-11-13 07:41:12 -0800

    Create mail.sh

---
+++ mail.sh
@@ -0,0 +1,6 @@
+apt-get update &&
+apt-get install bzip2 &&
+wget https://bitbucket.org/zhb/iredmail/downloads/iRedMail-0.8.5.tar.bz2 &&
+tar xjf iRedMail-0.8.5.tar.bz2 &&
+cd iRedMail-0.8.5/ &&
+bash iRedMail.sh
